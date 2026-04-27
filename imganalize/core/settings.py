14652ab2b3bc9595e145983d73cb2c1c62ae077f
Alfred_Joseph.W <alfredjoseph0405@gmail.com>
2026-04-27 20:44:12 +0530

    Making django security key random generated

--- imganalize/core/settings.py
+++ imganalize/core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7e+ry#53sokd%+=7w8uhz4bd+50knurra)uz3(-+m123=jk)#3'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
